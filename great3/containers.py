4a4a49b1762d7addceab3cd9c965b95474083a0d
Erin Sheldon <erin.sheldon@gmail.com>
2014-03-11 00:02:47 -0400

    added containers

---
+++ great3/containers.py
@@ -0,0 +1,75 @@
+"""
+Containers for great3 data for easier use
+"""
+from __future__ import print_function
+
+from . import files
+
+class Field(object):
+    def __init__(self, **keys):
+        """
+        parameters
+        ----------
+        See files.get_file.
+        
+        In short, experiment, obs_type, shear_type, subid, and optionally the
+        epoch.
+
+        the gal image and cat, and star image and cat are read.
+        """
+
+        self.cutout_size=48
+
+        self.load_data(**keys)
+
+    def get_gal_image(self, index):
+        """
+        Get the cutout centered on the indicated galaxy.
+        """
+        return self.get_image(index, 'gal')
+
+    def get_star_image(self, index):
+        """
+        Get the cutout centered on the indicated galaxy.
+        """
+        return self.get_image(index, 'star')
+
+
+    def get_image(self, index, type):
+        """
+        Get the cutout for the indicated index and object type.
+        """
+        
+        if type=='gal':
+            cat = self.gal_cat
+            im  = self.gal_image
+        else:
+            cat = self.star_cat
+            im  = self.star_image
+
+        row = cat['y'][index] 
+        col = cat['x'][index] 
+        row_low  = row - 23
+        row_high = row + 25
+        col_low  = col - 23
+        col_high = col + 25
+
+        cutout = im[row_low:row_high, col_low:col_high].copy()
+        cutout = numpy.array(cutout, dtype='f8', copy=False)
+
+        cen=[23.5, 23.5]
+        return cutout, cen
+
+    def load_data(self, **keys):
+        """
+        Load 
+            - gal image
+            - gal catalog
+            - star image
+            - star catalog
+        """
+
+        self.gal_image  = files.read_gal_image(**nkeys)
+        self.gal_cat    = files.read_gal_cat(**nkeys)
+        self.star_image = files.read_star_image(**nkeys)
+        self.star_cat   = files.read_star_cat(**nkeys)
