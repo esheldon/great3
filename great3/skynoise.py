85a40568dce2f6acea9e145ec7818fc337dcb6d2
Erin Sheldon <erin.sheldon@gmail.com>
2015-04-03 16:58:14 -0400

    work on one image at a time

--- great3/skynoise.py
+++ great3/skynoise.py
@@ -11,9 +11,10 @@ def calc_branch_sky_noise(**keys):
     """
     import fitsio
 
+    nsub=files.get_nsub(**keys)
+
     deep=keys.get('deep',False)
     if deep:
-        nsub=NSUB_DEEP
         outfile=files.get_deep_skynoise_file(**keys)
 
         fmin=-0.08
@@ -21,7 +22,6 @@ def calc_branch_sky_noise(**keys):
         binsize=0.001
 
     else:
-        nsub=NSUB
         outfile=files.get_skynoise_file(**keys)
         fmin=-0.3
         fmax= 0.15
@@ -34,33 +34,31 @@ def calc_branch_sky_noise(**keys):
     if not os.path.exists(d):
         os.makedirs(d)
 
-    out=numpy.zeros(nsub,dtype=[('subid','f8'),
-                                ('skysig','f8'),
-                                ('skysig_err','f8')])
+    out=numpy.zeros(1,dtype=[('subid','f8'),
+                             ('skysig','f8'),
+                             ('skysig_err','f8')])
 
-    for subid in xrange(nsub):
-        keys['subid']=subid
-        if deep:
-            im=files.read_deep_gal_image(**keys)
-            plot_file=files.get_deep_skynoise_plot_file(**keys)
-        else:
-            im=files.read_gal_image(**keys)
-            plot_file=files.get_skynoise_plot_file(**keys)
+    if deep:
+        im=files.read_deep_gal_image(**keys)
+        plot_file=files.get_deep_skynoise_plot_file(**keys)
+    else:
+        im=files.read_gal_image(**keys)
+        plot_file=files.get_skynoise_plot_file(**keys)
 
-        gf = get_sky_noise(im,fmin,fmax,binsize)
-        res=gf.get_result()
-        plt=gf.make_plot(show=False)
+    gf = get_sky_noise(im,fmin,fmax,binsize)
+    res=gf.get_result()
+    plt=gf.make_plot(show=False)
 
-        print("    writing:",plot_file)
-        plt.write_eps(plot_file)
+    print("    writing:",plot_file)
+    plt.write_eps(plot_file)
 
-        pars=res['pars']
-        perr=res['perr']
+    pars=res['pars']
+    perr=res['perr']
 
-        print('    %.3g +/- %.3g' % (pars[1],perr[1]))
-        out['subid'][subid]=subid
-        out['skysig'][subid]=pars[1]
-        out['skysig_err'][subid]=perr[1]
+    print('    %.3g +/- %.3g' % (pars[1],perr[1]))
+    out['subid']=keys['subid']
+    out['skysig']=pars[1]
+    out['skysig_err']=perr[1]
 
     print("writing:",outfile)
     fitsio.write(outfile, out, clobber=True)
